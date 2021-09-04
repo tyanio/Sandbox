0dc08b3664845ac43f3d18c04545cc4f37bd5fb4
tyanio <tyanio1996@gmail.com>
2021-09-05 04:03:01 +0900

    cuddの実験２を追加しました

--- cudd/experiment2.cc
+++ cudd/experiment2.cc
@@ -5,121 +5,44 @@
 
 using namespace std;
 
-void and_vars(int, BDD &, Cudd &, int);
-void or_vars(int, BDD &, Cudd &, int);
 void random_vars(int, BDD &, Cudd &, int);
 
 int main()
 {
-    int loops = 5;
+    const int nVariables = 10000;
+    const int loops = 5;
     auto msec = 0;
 
     Cudd cudd;
-    BDD f = cudd.bddVar();
-    BDD bdds[2] = {f, f};
+    for (int i = 0; i < nVariables; i++)
+    {
+        cudd.bddVar();
+    }
 
-    // for (int i = 0; i < 2; i++)
-    // {
-    //     int nVariables = 10000;
-    //     and_vars(nVariables, bdds[i], cudd, i);
-    // }
+    BDD f = cudd.bddVar(0);
+    for (int i = 1; i < nVariables; i++)
+    {
+        f = f * cudd.bddVar(i);
+    }
 
-    int nVariables = 10000;
-    random_vars(nVariables, bdds[0], cudd, 0);
-    random_vars(nVariables, bdds[1], cudd, 1);
+    // Cudd_PrintDebug(cudd.getManager(), f.getNode(), nVariables, 3);
 
     for (int i = 0; i < loops; i++)
     {
         auto start = chrono::system_clock::now();
-
-        BDD h = bdds[0] + !bdds[1];
-
-        auto end = chrono::system_clock::now();
-        auto dur = end - start;
-        cout << chrono::duration_cast<chrono::microseconds>(dur).count() << endl;
-        msec += chrono::duration_cast<chrono::microseconds>(dur).count();
-
-        // Cudd_PrintDebug(cudd.getManager(), h.getNode(), 1000, 3);
-    }
-
-    msec /= loops;
-    cout << msec << "micro sec \n";
-}
-
-void and_vars(int nVariables, BDD &f, Cudd &cudd, int loops)
-{
-    for (int i = 1; i < nVariables; i++)
-    {
-        if (loops == 0)
-        {
-            f = f * cudd.bddVar();
-        }
-        else
+        f = cudd.bddVar(0);
+        for (int i = 1; i < nVariables; i++)
         {
             f = f * cudd.bddVar(i);
         }
+        auto end = chrono::system_clock::now();
+        auto dur = end - start;
+        msec += chrono::duration_cast<chrono::microseconds>(dur).count();
+        // Cudd_PrintDebug(cudd.getManager(), f.getNode(), nVariables, 3);
     }
-}
 
-void or_vars(int nVariables, BDD &f, Cudd &cudd, int loops)
-{
-    for (int i = 1; i < nVariables; i++)
-    {
-        if (loops == 0)
-        {
-            f = f + cudd.bddVar();
-        }
-        else
-        {
-            f = f + cudd.bddVar(i);
-        }
-    }
-}
+    // Cudd_PrintDebug(cudd.getManager(), f.getNode(), nVariables, 3);
 
-void random_vars(int nVariables, BDD &f, Cudd &cudd, int loops)
-{
-    for (int i = 1; i < nVariables; i++)
-    {
-        auto x = rand();
-        if (loops == 0)
-        {
-            switch (x % 4)
-            {
-            case 0:
-                f = f * cudd.bddVar();
-                break;
-            case 1:
-                f = f + cudd.bddVar();
-                break;
-            case 2:
-                f = f * !(cudd.bddVar());
-                break;
-            case 3:
-                f = f + !(cudd.bddVar());
-                break;
-            default:
-                break;
-            }
-        }
-        else
-        {
-            switch (x % 4)
-            {
-            case 0:
-                f = f * cudd.bddVar(i);
-                break;
-            case 1:
-                f = f + cudd.bddVar(i);
-                break;
-            case 2:
-                f = f * !(cudd.bddVar(i));
-                break;
-            case 3:
-                f = f + !(cudd.bddVar(i));
-                break;
-            default:
-                break;
-            }
-        }
-    }
+    msec /= loops;
+    cout << "average: " << msec << " micro sec \n";
 }
