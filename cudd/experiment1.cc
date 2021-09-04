93df2d392d40aa8f1caf929fa9c7cc851c811f96
tyanio <tyanio1996@gmail.com>
2021-09-05 00:02:32 +0900

    より実験１で平等な条件で実装しました(randomの方は実装してません)

--- cudd/experiment1.cc
+++ cudd/experiment1.cc
@@ -5,97 +5,90 @@
 
 using namespace std;
 
-void and_vars(int, BDD &, Cudd &, int);
 void random_vars(int, BDD &, Cudd &, int);
 
 int main()
 {
-    int loops = 5;
+    const int nVariables = 10000;
+    const int loops = 1;
     auto msec = 0;
+
     Cudd cudd;
-    BDD f = cudd.bddVar();
+    for (int i = 0; i < nVariables; i++)
+    {
+        cudd.bddVar();
+    }
 
     for (int i = 0; i < loops; i++)
     {
-        int nVariables = 10000;
-        f = cudd.bddVar(0);
-
         auto start = chrono::system_clock::now();
 
-        and_vars(nVariables, f, cudd, i);
-        // random_vars(nVariables, f, cudd);
+        BDD f = cudd.bddVar(0);
+
+        for (int i = 1; i < nVariables; i++)
+        {
+            f = f * cudd.bddVar(i);
+        }
 
         auto end = chrono::system_clock::now();
+
+        // f.PrintCover();
+
         auto dur = end - start;
-        cout << chrono::duration_cast<chrono::microseconds>(dur).count() << endl;
+        // cout << chrono::duration_cast<chrono::microseconds>(dur).count() << endl;
         msec += chrono::duration_cast<chrono::microseconds>(dur).count();
 
         // Cudd_PrintDebug(cudd.getManager(), f.getNode(), nVariables, 3);
     }
 
     msec /= loops;
-    cout << "average: " << msec << "micro sec \n";
+    cout << "average: " << msec << " micro sec \n";
 }
 
-void and_vars(int nVariables, BDD &f, Cudd &cudd, int loops)
-{
-    for (int i = 1; i < nVariables; i++)
-    {
-        if (loops == 0)
-        {
-            f = f * cudd.bddVar();
-        }
-        else
-        {
-            f = f * cudd.bddVar(i);
-        }
-    }
-}
-
-void random_vars(int nVariables, BDD &f, Cudd &cudd, int loops)
-{
-    for (int i = 1; i < nVariables; i++)
-    {
-        auto x = rand();
-        if (loops == 0)
-        {
-            switch (x % 4)
-            {
-            case 0:
-                f = f * cudd.bddVar();
-                break;
-            case 1:
-                f = f + cudd.bddVar();
-                break;
-            case 2:
-                f = f * !(cudd.bddVar());
-                break;
-            case 3:
-                f = f + !(cudd.bddVar());
-                break;
-            default:
-                break;
-            }
-        }
-        else
-        {
-            switch (x % 4)
-            {
-            case 0:
-                f = f * cudd.bddVar(i);
-                break;
-            case 1:
-                f = f + cudd.bddVar(i);
-                break;
-            case 2:
-                f = f * !(cudd.bddVar(i));
-                break;
-            case 3:
-                f = f + !(cudd.bddVar(i));
-                break;
-            default:
-                break;
-            }
-        }
-    }
-}
+// void random_vars(int nVariables, BDD &f, Cudd &cudd, int loops)
+// {
+//     for (int i = 1; i < nVariables; i++)
+//     {
+//         auto x = rand();
+//         if (loops == 0)
+//         {
+//             switch (x % 4)
+//             {
+//             case 0:
+//                 f = f * cudd.bddVar();
+//                 break;
+//             case 1:
+//                 f = f + cudd.bddVar();
+//                 break;
+//             case 2:
+//                 f = f * !(cudd.bddVar());
+//                 break;
+//             case 3:
+//                 f = f + !(cudd.bddVar());
+//                 break;
+//             default:
+//                 break;
+//             }
+//         }
+//         else
+//         {
+//             switch (x % 4)
+//             {
+//             case 0:
+//                 f = f * cudd.bddVar(i);
+//                 break;
+//             case 1:
+//                 f = f + cudd.bddVar(i);
+//                 break;
+//             case 2:
+//                 f = f * !(cudd.bddVar(i));
+//                 break;
+//             case 3:
+//                 f = f + !(cudd.bddVar(i));
+//                 break;
+//             default:
+//                 break;
+//             }
+//         }
+//     }
+// }
